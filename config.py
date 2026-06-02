39df7e2df3ef828400a77e00cf2f4746ab52196c
seven-7c <2176895070@qq.com>
2026-06-02 16:12:59 +0800

    Add configuration file

---
+++ config.py
@@ -0,0 +1,7 @@
+MODEL_NAME = "deepseek-r1:7b"
+EMBED_MODEL_NAME = "nomic-embed-text"
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
+TOP_K = 3
+VECTOR_DB_PATH = "./chroma_db"
+DOCUMENTS_FOLDER = "./documents"
